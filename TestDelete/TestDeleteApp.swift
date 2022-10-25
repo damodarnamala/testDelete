cdf1ae6951407eb5b566a6305e180304882354ad
Damodar <damodar.namala@outlook.com>
2022-10-25 15:56:45 +0530

    Initial Commit

---
+++ TestDelete/TestDeleteApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestDeleteApp.swift
+//  TestDelete
+//
+//  Created by Damodar Namala on 25/10/22.
+//
+
+import SwiftUI
+
+@main
+struct TestDeleteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
